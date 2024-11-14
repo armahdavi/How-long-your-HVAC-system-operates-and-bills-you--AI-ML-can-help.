258f6d0753cd6beeed1493a50ad8d863bb5c8d80
Alireza <ar.mahdavi21@gmail.com>
2024-11-14 16:21:03 -0500

    Add files via upload

---
+++ runtime_feature_engineering.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 14 15:31:54 2024
+
+@author: alima
+"""
+
+import numpy as np
+import pandas as pd
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import GridSearchCV, PredefinedSplit
+from sklearn.neighbors import KNeighborsClassifier
+import warnings
+warnings.filterwarnings('ignore')  
+exec(open(r'C:\Life\5- Career & Business Development\Learning\Python Practice\Generic Codes\notion_corrections.py').read())
+
+########################################
+### Step 1: Data ETL for ML Modeling ###
+########################################
+df = pd.read_excel(backslash_correct(r'C:\Life\5- Career & Business Development\Learning\Python Practice\Stata_Python_Booster\PhD - QFF\Processed\ML\rt_data_master.xlsx'))
+df = df[df['visit'] < 5] 
+## The last two visits (5 and 6 are excluded as representing unusual cases: 
+## HVAC was intentionally running continously with fan only mode to collect more dust for QFF)
+print(len(df))
+
+df['Mode'].unique() # Correction to y by filling transient with ffil
+df.replace('Transient', np.nan, inplace = True)
+
+with warnings.catch_warnings(record=True) as w:
+    df.fillna(method = 'ffill', inplace = True)
+
+df['Mode'] = np.where(df['Mode'] == 'Compressor', 1, 0)
+
+X = df[['Temp', 'RH', 'Temp_1st_der', 'Temp_2nd_der',
+        'RH_1st_der', 'RH_2nd_der', 'CO2 In', 'CO2 Out']]
+
+
+####################################
+### Step 2: Correlational Matrix ###
+####################################
+correlation_matrix = X.corr()
+
+plt.figure(figsize=(10, 8))
+sns.heatmap(correlation_matrix, annot = True, cmap = 'coolwarm', 
+            square = True, fmt = ".2f", cbar_kws = {"shrink": .8})
+plt.title('Correlation Matrix of HVAC Runtime Features', fontsize = 14)
+plt.show()
+
+
+####################################################################
+### Step 3: Scatter Matrix and Kernel Density Estimations (KDEs) ###
+####################################################################
+color_map = df['Mode'].map({0: 'blue', 1: 'red'})
+scatter_matrix(X, alpha=0.8, figsize=(12, 12), diagonal='kde', marker='o', c=color_map)
+plt.suptitle('Scatter Matrix of Features', fontsize = 20)
+plt.show()
+
+for col in X.columns:
+    plt.figure(figsize=(10, 6))
+    sns.kdeplot(df[df['Mode'] == 0][col], color='blue', fill=True, alpha=0.3, label = 'Compressor OFF')
+    sns.kdeplot(df[df['Mode'] == 1][col], color='red', fill=True, alpha=0.3, label = 'Compressor ON')
+    
+    plt.title(f'Feature: {col}', fontsize = 14)
+    plt.xlabel("Feature Values",  fontsize = 14)  
+    plt.ylabel("Density", fontsize = 14)
+
+    plt.legend()
+    plt.show()
+
+## Save the features and target for later ML modeling
+pd.concat([X, df['Mode']], axis = 1).to_excel(r'C:\Life\5- Career & Business Development\Learning\Python Practice\Stata_Python_Booster\PhD - QFF\Processed\ML\rt_data_master_ml.xlsx')
